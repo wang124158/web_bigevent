19d286f0d7dcdc5ebd0fd4e1053bd9195a8f7c81
itheima <1241581704@qq.com>
2021-03-01 19:06:04 +0800

    完成了登陆和注册开发

---
+++ assets/js/login.js
@@ -0,0 +1,74 @@
+$(function () {
+    // 点击“去注册账号”的链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    // 点击“去登录”的链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+})
+
+// 注册的表单
+$('#form_reg').on('submit', function (e) {
+    // 1. 阻止默认的提交行为
+    e.preventDefault()
+    // 2. 发起Ajax的POST请求
+    var data = {
+        username: $('#form_reg [name=username]').val(),
+        password: $('#form_reg [name=password]').val()
+    }
+    $.post('/api/reguser', data, function (res) {
+        if (res.status !== 0) {
+            return layer.msg(res.message)
+        }
+        layer.msg('注册成功，请登录！')
+        // 模拟人的点击行为
+        $('#link_login').click()
+    })
+})
+
+// 登陆的表单
+$('#form_login').on('submit', function (e) {
+    e.preventDefault()
+    $.ajax({
+        url: '/api/login',
+        method: 'POST',
+        data: $(this).serialize(),
+        success: function (res) {
+            if (res.status !== 0) {
+                return layer.msg('登陆失败')
+            }
+            layer.msg('登陆成功')
+            // console.log(res.token);
+            location.href = 'index.html'
+        }
+    })
+
+})
+
+
+
+
+
+
+
+var form = layui.form
+form.verify({
+    // 自定义了一个叫做 pwd 校验规则
+    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    // 校验两次密码是否一致的规则
+    repwd: function (value) {
+        // 通过形参拿到的是确认密码框中的内容
+        // 还需要拿到密码框中的内容
+        // 然后进行一次等于的判断
+        // 如果判断失败,则return一个提示消息即可
+        var pwd = $('.reg-box [name=password]').val()
+        if (pwd !== value) {
+            return '两次密码不一致！'
+        }
+    }
+})
